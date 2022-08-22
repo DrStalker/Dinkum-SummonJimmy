905e40f7538bbdb42334ec4e53efa58d29030ab1
Doug <doug.stalker@gmail.com>
2022-08-22 17:08:00 +1000

    Added a bunch of files to .gitignore

--- obj/Release/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SummonJimmy
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "Nep.SummonJimmy";
-        public const string PLUGIN_NAME = "Nep.SummonJimmy";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
