f435164e747b25efa1e899478244c9c036f9bd21
Doug <doug.stalker@gmail.com>
2022-08-22 18:12:10 +1000

    Removed despawn hotkey Removed ability to move Jimmy This fixes the issue of the game looping forever when saving if Jimmy has been summoned 9 or more times.

--- Plugin.cs
+++ Plugin.cs
@@ -12,12 +12,12 @@ namespace SummonJimmy;
 public class Plugin : BaseUnityPlugin
 {
     private readonly ConfigEntry<KeyCode> _hotKey;
-    private readonly ConfigEntry<KeyCode> _hotKeyDisable;
+    //private readonly ConfigEntry<KeyCode> _hotKeyDisable;
     public Plugin()
     {
 
     _hotKey        = Config.Bind("General", "HotKey",        KeyCode.F10,"Press to summon Jimmy");
-    _hotKeyDisable = Config.Bind("General", "HotKeyDisable", KeyCode.F11,"Press to dismiss Jimmy");
+    //_hotKeyDisable = Config.Bind("General", "HotKeyDisable", KeyCode.None,"Press to dismiss Jimmy");
     }
     
     private void Awake()
@@ -30,15 +30,23 @@ public class Plugin : BaseUnityPlugin
     {
         if (Input.GetKeyDown(_hotKey.Value))
         {
-            MarketPlaceManager.manage.despawnJimmiesBoat();
+            // Is Jimmy already here?
+            for (int i = 0; i < SaveLoad.saveOrLoad.vehiclesToSave.Count; i++)
+            {
+                if (SaveLoad.saveOrLoad.vehiclesToSave[i].saveId == 10)
+                {
+                    return;
+                }
+            }
+            // No Jimmy (or at least no boat for him) so summon him
             MarketPlaceManager.manage.spawnJimmysBoat();
-
         }
+        /*
         else if (Input.GetKeyDown(_hotKeyDisable.Value))
         {
             MarketPlaceManager.manage.despawnJimmiesBoat();
         }
-
+        */
     }
 
 
