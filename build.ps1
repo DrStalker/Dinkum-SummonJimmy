d72f80478439dc9a7f6aa93b3d98cf18a851b76f
Doug <doug.stalker@gmail.com>
2022-08-23 15:15:39 +1000

    added simple (and brainless) build script

---
+++ build.ps1
@@ -0,0 +1,2 @@
+dotnet build
+Copy-Item  -Path J:\dinkum\SummonJimmy\bin\Debug\netstandard2.0\Nep.SummonJimmy.dll -Destination C:\Games\Steam\steamapps\common\Dinkum\BepInEx\plugins\ -Force -Verbose
