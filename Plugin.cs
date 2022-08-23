06fb50b21e558b69fdb6d583299ea1f1bace4f2d
Doug <doug.stalker@gmail.com>
2022-08-23 15:13:53 +1000

    added notification messages

--- Plugin.cs
+++ Plugin.cs
@@ -28,16 +28,18 @@ public class Plugin : BaseUnityPlugin
     {
         if (Input.GetKeyDown(_hotKey.Value))
         {
-            // Is Jimmy already here?
+            // Is Jimmy already here?  Check to see if his boat is on the map. (this check is copied from the main game code)
             for (int i = 0; i < SaveLoad.saveOrLoad.vehiclesToSave.Count; i++)
             {
                 if (SaveLoad.saveOrLoad.vehiclesToSave[i].saveId == 10)
                 {
+                    NotificationManager.manage.createChatNotification("Jimmy is already somewhere on the island.");
                     return;
                 }
             }
             // No Jimmy (or at least no boat for him) so summon him
             MarketPlaceManager.manage.spawnJimmysBoat();
+            NotificationManager.manage.createChatNotification("Jimmy has been summoned!");
         }
     }
 
